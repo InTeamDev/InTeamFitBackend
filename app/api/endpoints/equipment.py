dc0e447dc3a787b9a71d2589713160ad57e701c9
ZetoOfficial <tpptgs@bk.ru>
2023-12-16 22:31:36 +0500

    remove try catch

--- app/api/endpoints/equipment.py
+++ app/api/endpoints/equipment.py
@@ -13,14 +13,14 @@ equipment_router = APIRouter()
 
 @equipment_router.post("/equipment/predict", response_model=list[Equipment])
 async def predict_image(file: UploadFile = File(...), model_manager: ModelManager = Depends(get_model_manager)):
-    try:
-        contents = await file.read()
-        image = Image.open(io.BytesIO(contents))
-        image = image.resize((200, 200))
-        image_array = np.array(image).astype(float)
-        # модель работает с числами от 0 до 1, поэтому все значения приведём к этому диапазону
-        image_array = image_array / 255.0
-        image_array = np.expand_dims(image_array, axis=0)  # превратим изображение в тензор
-        return await EquipmentService.recognize(image_array, model_manager.get_model())
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    if file.content_type != "image/jpeg":
+        raise HTTPException(status_code=400, detail="File must be a JPEG")
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))
+    image.save("image.jpg")
+    image = image.resize((200, 200))
+    image_array = np.array(image).astype(float)
+    # модель работает с числами от 0 до 1, поэтому все значения приведём к этому диапазону
+    image_array = image_array / 255.0
+    image_array = np.expand_dims(image_array, axis=0)  # превратим изображение в тензор
+    return await EquipmentService.recognize(image_array, model_manager.get_model())
